acda02dc4688aac4ab774600382a4e1daab09adf
Teofilo Correa Rojas <teofilocorrea@gmail.com>
2025-07-31 12:14:53 -0400

    update: agregar scripts de creación de esquemas para estructuración modular
    Se incluyen los comandos `CREATE SCHEMA` para definir la organización lógica del modelo de base de datos, segmentando por áreas como gestión estudiantil, programas académicos, cursos e inscripciones.
    
    Este cambio mejora la claridad, el mantenimiento y la gobernanza de objetos dentro del proyecto SQL Server.

---
+++ scripts_sqlserver/02_schemas/create_schema_academicsystem.sql
@@ -0,0 +1,23 @@
+-- ========================================
+-- Archivo: 01_schema_creation.sql
+-- Descripción: Crea los esquemas principales
+--              (student_management, academic_programs, course_catalog, course_registration)
+--              utilizando MS_Description para propósitos técnicos y de portafolio.
+-- Autor: Teófilo Correa Rojas
+-- Fecha: 30 de julio de 2025
+-- ========================================
+
+USE AcademicSystem;
+GO
+
+CREATE SCHEMA student_management;
+GO
+
+CREATE SCHEMA academic_programs;
+GO
+
+CREATE SCHEMA course_catalog;
+GO
+
+CREATE SCHEMA course_registration;
+GO
